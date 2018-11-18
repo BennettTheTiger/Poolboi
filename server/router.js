6bcac4bdacea3ca3dc2841aaf564faa7fa81f96e
BennettTheTiger <bas9386@g.rit.edu>
2018-11-18 16:57:42 -0500

    tidy up router.js

--- server/router.js
+++ server/router.js
@@ -5,7 +5,10 @@ const router = (app) => {
   // core
   app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
   app.get('/about', (req, res) => { res.render('about'); });
-  app.get('/dashboard', mid.requiresSecure, mid.requiresLogin, (req, res) => { res.render('app'); });
+  app.get('/dashboard', mid.requiresSecure, mid.requiresLogin,
+    (req, res) => {
+      res.render('app');
+    });
   // Login routes
   app.get('/getToken', mid.requiresSecure, controllers.Account.getToken);
   app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
@@ -13,22 +16,27 @@ const router = (app) => {
   app.get('/logout', mid.requiresLogin, controllers.Account.logout);
   app.get('/accountInfo', mid.requiresSecure, mid.requiresLogin, controllers.Account.getBasic);
   // Password
-  app.get('/newPassword', mid.requiresLogin, mid.requiresSecure, (req, res) => { res.render('password'); });
-  app.post('/updatePass', mid.requiresLogin, mid.requiresSecure, controllers.Account.updatePassword);
+  app.get('/newPassword', mid.requiresLogin, mid.requiresSecure,
+  (req, res) => { res.render('password'); });
+  app.post('/updatePass',
+  mid.requiresLogin, mid.requiresSecure, controllers.Account.updatePassword);
   // weather
   app.get('/weather', controllers.Weather.getWeather);
   app.get('/forcast', controllers.Weather.getForcast);
   // water body
-  app.get('/newWaterBody', mid.requiresSecure, mid.requiresLogin, (req, res) => { res.render('newBody'); });
+  app.get('/newWaterBody', mid.requiresSecure, mid.requiresLogin,
+  (req, res) => { res.render('newBody'); });
   app.post('/addWaterBody', mid.requiresLogin, mid.requiresSecure, controllers.WaterBody.makeBody);
   app.get('/waterBodies', mid.requiresLogin, mid.requiresSecure, controllers.WaterBody.getBodies);
   // water samples
-  app.get('/newWaterTest', mid.requiresSecure, mid.requiresLogin, (req, res) => { res.render('addWater'); });
+  app.get('/newWaterTest', mid.requiresSecure, mid.requiresLogin,
+  (req, res) => { res.render('addWater'); });
   app.post('/addWater', mid.requiresLogin, mid.requiresSecure, controllers.WaterSample.makeSample);
   app.get('/addWater', mid.requiresLogin, mid.requiresSecure, controllers.WaterSample.getSamples);
   // error handling
   app.get('/error', (req, res) => { res.render('error', { msg: req.body }); });
-  app.get('*', (req, res) => { res.render('error', { msg: 'The page you are looking was not found' }); });
+  app.get('*',
+  (req, res) => { res.render('error', { msg: 'The page you are looking was not found' }); });
 };
 
 module.exports = router;
